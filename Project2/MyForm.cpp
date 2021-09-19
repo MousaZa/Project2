8a8b2442a9ab3f56641d0ed264a5bc07fe0d885d
ST01 <ST01@DESKTOP-SN3D7G5>
2021-09-19 16:12:27 +0300

    Add project files.

---
+++ Project2/MyForm.cpp
@@ -0,0 +1,18 @@
+#include "MyForm.h"
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+void main(array<String^>^ args)
+
+{
+
+    Application::EnableVisualStyles();
+
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Application::Run(gcnew Project2::MyForm);
+
+}
